71cfb01c4cc9710278dcd5150a5735ebbab42bdb
Eric Manning <emanningmusic@yahoo.com>
2019-10-14 05:02:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,11 @@ while  seconds_to_midnight > 0
  "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep = 5
-def number(countdown_with_sleep)
-  while countdown_with_sleep > 0 
-  puts "#{countdown_with_sleep} SECOND(S)"
-  countdown_with_sleep -= 1 
+number = 5
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)"
+  number -= 1 
   sleep 1 
 end
 "HAPPY NEW YEAR!"
