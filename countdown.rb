41363bfc77c161d6b6f886fa412935548714a3a4
Eric Manning <emanningmusic@yahoo.com>
2019-10-14 04:54:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
 
-def countdown
+
+seconds_to_midnight = 5
+def countdown(seconds_to_midnight)
+while  seconds_to_midnight > 0
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   seconds_to_midnight -= 1
+   sleep 1
+ end
+ "HAPPY NEW YEAR!"
 end
+
